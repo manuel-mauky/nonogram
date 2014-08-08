0f41ff014df7661e8aef2a9f9ede85376901b526
Manuel Mauky <manuel.mauky@googlemail.com>
2014-08-09 00:04:40 +0200

    Integrated Grids

---
+++ src/main/java/eu/lestard/nonogram/core/Numbers.java
@@ -0,0 +1,15 @@
+package eu.lestard.nonogram.core;
+
+public enum Numbers {
+
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE
+
+}
--- src/main/java/eu/lestard/nonogram/puzzle/PuzzleView.java
+++ src/main/java/eu/lestard/nonogram/puzzle/PuzzleView.java
@@ -1,26 +1,34 @@
 package eu.lestard.nonogram.puzzle;
 
 import de.saxsys.jfx.mvvm.api.FxmlView;
+import eu.lestard.grid.GridModel;
+import eu.lestard.grid.GridView;
+import eu.lestard.nonogram.core.State;
 import javafx.beans.binding.DoubleBinding;
 import javafx.beans.value.ObservableDoubleValue;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 public class PuzzleView implements FxmlView<PuzzleViewModel> {
 
+    private static final int COLUMNS = 20;
+    private static final int ROWS = 20;
+
+
 
     @FXML
-    private Pane centerPane;
+    private AnchorPane centerPane;
 
     @FXML
-    private Pane overviewPane;
+    private AnchorPane overviewPane;
 
     @FXML
-    private Pane topNumberPane;
+    private AnchorPane topNumberPane;
 
     @FXML
-    private Pane leftNumberPane;
+    private AnchorPane leftNumberPane;
 
     @FXML
     private AnchorPane rootPane;
@@ -28,6 +36,63 @@ public class PuzzleView implements FxmlView<PuzzleViewModel> {
 
     public void initialize(){
 
+        initLayout();
+
+        initCenterGrid();
+
+        initTopNumberGrid();
+
+        initLeftNumberGrid();
+    }
+
+
+    private void initLeftNumberGrid() {
+        GridModel<State> leftNumberGridModel = new GridModel<>();
+        leftNumberGridModel.setNumberOfColumns(COLUMNS/2);
+        leftNumberGridModel.setNumberOfRows(ROWS);
+
+        GridView<State> leftNumberGridView = new GridView<>();
+        leftNumberGridView.setGridModel(leftNumberGridModel);
+        leftNumberPane.getChildren().add(leftNumberGridView);
+
+        initAnchor(leftNumberGridView);
+    }
+
+    private void initTopNumberGrid() {
+        GridModel<State> topNumberGridModel = new GridModel<>();
+        topNumberGridModel.setNumberOfColumns(COLUMNS);
+        topNumberGridModel.setNumberOfRows(ROWS/2);
+
+        GridView<State> topNumberGridView = new GridView<>();
+        topNumberGridView.setGridModel(topNumberGridModel);
+        topNumberPane.getChildren().add(topNumberGridView);
+
+        initAnchor(topNumberGridView);
+    }
+
+    private void initCenterGrid() {
+        GridModel<State> centerGridModel = new GridModel<>();
+
+        centerGridModel.setNumberOfColumns(COLUMNS);
+        centerGridModel.setNumberOfRows(ROWS);
+
+        GridView<State> centerGridView = new GridView<>();
+        centerGridView.setGridModel(centerGridModel);
+
+        centerPane.getChildren().add(centerGridView);
+        initAnchor(centerGridView);
+    }
+
+    private void initAnchor(Node node){
+        AnchorPane.setBottomAnchor(node, 0.0);
+        AnchorPane.setTopAnchor(node, 0.0);
+        AnchorPane.setLeftAnchor(node, 0.0);
+        AnchorPane.setRightAnchor(node, 0.0);
+    }
+
+
+
+    private void initLayout() {
         final DoubleBinding oneThirdsWidth = rootPane.widthProperty().divide(3);
         final DoubleBinding oneThirdsHeight = rootPane.heightProperty().divide(3);
 
@@ -40,10 +105,8 @@ public class PuzzleView implements FxmlView<PuzzleViewModel> {
         bindWidth(topNumberPane, twoThirdsWidth);
         bindHeight(topNumberPane, oneThirdsHeight);
 
-
         bindWidth(leftNumberPane, oneThirdsWidth);
         bindHeight(leftNumberPane, twoThirdsHeight);
-
 
         bindWidth(centerPane, twoThirdsWidth);
         bindHeight(centerPane, twoThirdsHeight);
@@ -56,7 +119,6 @@ public class PuzzleView implements FxmlView<PuzzleViewModel> {
 
         AnchorPane.setBottomAnchor(leftNumberPane, 0.0);
         AnchorPane.setLeftAnchor(leftNumberPane, 0.0);
-
 
         AnchorPane.setBottomAnchor(centerPane,0.0);
         AnchorPane.setRightAnchor(centerPane,0.0);
