bf4f324395be8654a0d48fae25324ea3d0b716ff
Manuel Mauky <manuel.mauky@googlemail.com>
2014-08-21 23:26:22 +0200

    Refactored internal gridModel of puzzle class

--- src/main/java/eu/lestard/nonogram/core/Puzzle.java
+++ src/main/java/eu/lestard/nonogram/core/Puzzle.java
@@ -11,17 +11,12 @@ public class Puzzle {
     private final int size;
 
 
-    private static enum PuzzleFill {
-        EMPTY,
-        FILLED
-    }
-
-    private GridModel<PuzzleFill> gridModel = new GridModel<>();
+    private GridModel<Boolean> gridModel = new GridModel<>();
 
     public Puzzle(int size) {
         this.size = size;
 
-        gridModel.setDefaultState(PuzzleFill.EMPTY);
+        gridModel.setDefaultState(false);
 
         gridModel.setNumberOfColumns(size);
         gridModel.setNumberOfRows(size);
@@ -29,12 +24,12 @@ public class Puzzle {
 
     public void addPoint(int columnNr, int rowNr) {
         if (columnNr >= 0 && columnNr < size && rowNr >= 0 && rowNr < size) {
-            gridModel.getCell(columnNr, rowNr).changeState(PuzzleFill.FILLED);
+            gridModel.getCell(columnNr, rowNr).changeState(true);
         }
     }
 
     public boolean isPoint(int column, int row) {
-        return gridModel.getCell(column,row).getState() == PuzzleFill.FILLED;
+        return gridModel.getCell(column,row).getState();
     }
 
     public int getSize() {
@@ -50,18 +45,15 @@ public class Puzzle {
     }
 
 
-    private List<Integer> getNumberBlocks(List<Cell<PuzzleFill>> cells) {
+    private List<Integer> getNumberBlocks(List<Cell<Boolean>> cells) {
         List<Integer> numbers = new ArrayList<>();
 
         int tmp = 0;
 
-        for (Cell<PuzzleFill> cell : cells) {
-            if(cell.getState() == PuzzleFill.FILLED){
+        for (Cell<Boolean> cell : cells) {
+            if(cell.getState()){
                 tmp++;
-                continue;
-            }
-
-            if(cell.getState() == PuzzleFill.EMPTY && tmp > 0){
+            } else if(tmp > 0){
                 numbers.add(tmp);
                 tmp = 0;
             }
